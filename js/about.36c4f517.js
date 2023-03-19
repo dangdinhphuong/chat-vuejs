55c9a9906de0bbc4b9443d2d2474db3d75230587
dang dinh phuong <dangxuong2000@gmail.com>
2023-03-19 18:30:32 +0700

    Updates

---
+++ js/about.36c4f517.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkchat_vuejs"]=self["webpackChunkchat_vuejs"]||[]).push([[443],{7381:function(n,u,t){t.r(u),t.d(u,{default:function(){return f}});var a=t(3396);const e={class:"about"},s=(0,a._)("h1",null,"This is an about page",-1),c=[s];function r(n,u){return(0,a.wg)(),(0,a.iD)("div",e,c)}var i=t(89);const h={},o=(0,i.Z)(h,[["render",r]]);var f=o}}]);
+//# sourceMappingURL=about.36c4f517.js.map
